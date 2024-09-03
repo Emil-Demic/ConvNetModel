0fe809eeb20a0feddcab9f6b290a8801b45f8230
Emil-Demic <77987406+Emil-Demic@users.noreply.github.com>
2024-09-03 16:44:05 +0200

    dataset useses fscoco

--- data.py
+++ data.py
@@ -1,33 +1,45 @@
 import os
 import random
 
-import cv2
-import numpy
 from PIL import Image
 from torch.utils.data import Dataset
 
-
 class DatasetTrain(Dataset):
-    def __init__(self, root, transforms_sketch=None, transforms_image=None):
+    def __init__(self, root, num_of_users=100, transforms_sketch=None, transforms_image=None):
+        if num_of_users > 100:
+            raise ValueError('num_of_users > 100')
+
         self.root = root
-        self.sketches = os.listdir(os.path.join(self.root, "sketch", "Image"))
-        self.images = os.listdir(os.path.join(self.root, "image", "Image"))
+
+        with open(os.path.join(self.root, 'val_unseen_user.txt'), 'r') as f:
+            lines = f.readlines()
+            val_ids = list(map(int, lines))
+
+        self.files = []
+        for i in range(1, num_of_users + 1):
+            file_names = os.listdir(os.path.join(root, "images", str(i)))
+            file_names = [file.split('.')[0] for file in file_names]
+            for file_name in file_names:
+                if file_name not in val_ids:
+                    self.files.append(os.path.join(str(i), file_name))
+
         self.transforms_sketch = transforms_sketch
         self.transforms_image = transforms_image
+
         random.seed(42)
 
     def __len__(self):
-        return len(self.images)
+        return len(self.files)
 
     def __getitem__(self, idx):
-        sketch_path = os.path.join(self.root, "sketch", "Image", self.sketches[idx])
-        image_path = os.path.join(self.root, "image", "Image", self.images[idx])
+        sketch_path = os.path.join(self.root, "raster_sketches", self.files[idx] + ".jpg")
+        image_path = os.path.join(self.root, "images", self.files[idx] + ".jpg")
 
-        negative_idx = random.randint(0, len(self.sketches) - 1)
+        negative_idx = random.randint(0, len(self.files) - 1)
         while negative_idx == idx:
-            negative_idx = random.randint(0, len(self.sketches) - 1)
+            negative_idx = random.randint(0, len(self.files) - 1)
 
-        negative_path = os.path.join(self.root, "image", "Image", self.images[negative_idx])
+        negative_path = os.path.join(self.root, "images", self.files[negative_idx] + ".jpg")
 
         sketch = Image.open(sketch_path)
         image = Image.open(image_path).convert('RGB')
@@ -42,22 +54,89 @@ class DatasetTrain(Dataset):
 
         return sketch, image, negative
 
-
 class DatasetTest(Dataset):
-    def __init__(self, img_dir, transforms=None):
-        self.img_dir = img_dir
-        self.images = os.listdir(img_dir)
+    def __init__(self, root, num_of_users=100, transforms=None):
+        if num_of_users > 100:
+            raise ValueError('num_of_users > 100')
+
+        self.root = root
+
+        with open(os.path.join(self.root, "..", 'val_unseen_user.txt'), 'r') as f:
+            lines = f.readlines()
+            val_ids = list(map(int, lines))
+
+        self.files = []
+        for i in range(1, num_of_users + 1):
+            file_names = os.listdir(os.path.join(root, str(i)))
+            file_names = [file.split('.')[0] for file in file_names]
+            for file_name in file_names:
+                if file_name in val_ids:
+                    self.files.append(os.path.join(str(i), file_name))
+
         self.transforms = transforms
 
     def __len__(self):
-        return len(self.images)
+        return len(self.files)
 
     def __getitem__(self, idx):
-        img_path = os.path.join(self.img_dir, self.images[idx])
+        img_path = os.path.join(self.root, self.files[idx] + ".jpg")
         img = Image.open(img_path)
         if self.transforms is not None:
             img = self.transforms(img)
 
         return img
+
+# class DatasetTrain(Dataset):
+#     def __init__(self, root, transforms_sketch=None, transforms_image=None):
+#         self.root = root
+#         self.sketches = os.listdir(os.path.join(self.root, "sketch", "Image"))
+#         self.images = os.listdir(os.path.join(self.root, "image", "Image"))
+#         self.transforms_sketch = transforms_sketch
+#         self.transforms_image = transforms_image
+#         random.seed(42)
+#
+#     def __len__(self):
+#         return len(self.images)
+#
+#     def __getitem__(self, idx):
+#         sketch_path = os.path.join(self.root, "sketch", "Image", self.sketches[idx])
+#         image_path = os.path.join(self.root, "image", "Image", self.images[idx])
+#
+#         negative_idx = random.randint(0, len(self.sketches) - 1)
+#         while negative_idx == idx:
+#             negative_idx = random.randint(0, len(self.sketches) - 1)
+#
+#         negative_path = os.path.join(self.root, "image", "Image", self.images[negative_idx])
+#
+#         sketch = Image.open(sketch_path)
+#         image = Image.open(image_path).convert('RGB')
+#         negative = Image.open(negative_path).convert('RGB')
+#
+#         if self.transforms_sketch:
+#             sketch = self.transforms_sketch(sketch)
+#
+#         if self.transforms_image:
+#             image = self.transforms_image(image)
+#             negative = self.transforms_image(negative)
+#
+#         return sketch, image, negative
+
+
+# class DatasetTest(Dataset):
+#     def __init__(self, img_dir, transforms=None):
+#         self.img_dir = img_dir
+#         self.images = os.listdir(img_dir)
+#         self.transforms = transforms
+#
+#     def __len__(self):
+#         return len(self.images)
+#
+#     def __getitem__(self, idx):
+#         img_path = os.path.join(self.img_dir, self.images[idx])
+#         img = Image.open(img_path)
+#         if self.transforms is not None:
+#             img = self.transforms(img)
+#
+#         return img
 
 
