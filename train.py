08e76af1f5a8786c64d1b0ef5240d33f98a4c798
Emil-Demic <77987406+Emil-Demic@users.noreply.github.com>
2024-09-06 14:18:43 +0200

    bug fix

--- train.py
+++ train.py
@@ -27,8 +27,8 @@ transforms = Compose([
 ])
 
 dataset_train = DatasetTrain("fscoco", args.users, transforms, transforms)
-# dataset_test_sketch = DatasetTest("fscoco/raw_data", True, args.users, transforms)
-dataset_test_sketch = DatasetTest("fscoco/raster_sketches", False, args.users, transforms)
+dataset_test_sketch = DatasetTest("fscoco/raw_data", True, args.users, transforms)
+# dataset_test_sketch = DatasetTest("fscoco/raster_sketches", False, args.users, transforms)
 dataset_test_image = DatasetTest("fscoco/images", False, args.users, transforms)
 
 if args.cuda:
--- utils.py
+++ utils.py
@@ -83,7 +83,7 @@ def drawPNG(vector_images, side=256, time_frac=None, skip_front=False, add_strok
     raster_image = cv2.applyColorMap(raster_image, cv2.COLORMAP_JET)
     raster_image[mask] = 255
 
-    cv2.imshow('raster_image', raster_image)
-    cv2.waitKey(0)
+    # cv2.imshow('raster_image', raster_image)
+    # cv2.waitKey(0)
 
     return raster_image
