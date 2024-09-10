6fa9a598409177c4753ac21e76463e09c9bad93e
Emil-Demic <77987406+Emil-Demic@users.noreply.github.com>
2024-09-10 11:36:13 +0200

    Update data.py

--- data.py
+++ data.py
@@ -65,7 +65,7 @@ class DatasetTrain(Dataset):
 
         c = self.rng.choice([True, False])
         if c:
-            sketch = drawPNG(json.load(open(sketch_path)), skip_front=True, time_frac=0.02)
+            sketch = drawPNG(json.load(open(sketch_path)), add_stroke=True)
         else:
             sketch = drawPNG(json.load(open(sketch_path)))
 
