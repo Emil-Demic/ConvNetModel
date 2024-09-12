f3372709baf012351b31e2237d5ddab497384fb8
Emil-Demic <77987406+Emil-Demic@users.noreply.github.com>
2024-09-12 11:47:31 +0200

    Update train.py

--- train.py
+++ train.py
@@ -34,7 +34,7 @@ dataset_train = DatasetTrain("fscoco", args.users, transforms, transforms)
 if args.colormap:
     dataset_test_sketch = DatasetTest("fscoco/raw_data", True, args.users, transforms)
 else:
-    dataset_test_sketch = DatasetTest("fscoco/raw_data", True, args.users, transforms)
+    dataset_test_sketch = DatasetTest("fscoco/raster_sketches", False, args.users, transforms)
 dataset_test_image = DatasetTest("fscoco/images", False, args.users, transforms)
 
 if args.cuda:
