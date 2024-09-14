04ee12473081ab88fe42a44297212edb0ac8c51e
Emil-Demic <77987406+Emil-Demic@users.noreply.github.com>
2024-09-14 22:39:19 +0200

    Update data.py

--- data.py
+++ data.py
@@ -47,25 +47,24 @@ class DatasetTrain(Dataset):
 
         negative_path = os.path.join(self.root, "images", self.files[negative_idx] + ".jpg")
 
-        selection = self.rng.choice([1, 2, 3, 4], p=[0.4, 0.2, 0.2, 0.2])
-        # amount = self.rng.random() % 0.1
-        # match selection:
-        #     case 1:
-        #         sketch = drawPNG(json.load(open(sketch_path)))
-        #     case 2:
-        #         sketch = drawPNG(json.load(open(sketch_path)), skip_front=True, time_frac=0.02)
-        #     case 3:
-        #         sketch = drawPNG(json.load(open(sketch_path)), skip_front=False, time_frac=0.05)
-        #     case 4:
-        #         sketch = drawPNG(json.load(open(sketch_path)), add_stroke=True)
-        #     case _:
-        #         sketch = drawPNG(json.load(open(sketch_path)))
+        selection = self.rng.choice([1, 2, 3], p=[0.6, 0.2, 0.2])
+        match selection:
+            case 1:
+                sketch = drawPNG(json.load(open(sketch_path)))
+            case 2:
+                sketch = drawPNG(json.load(open(sketch_path)), skip_front=True, time_frac=0.01)
+            case 3:
+                sketch = drawPNG(json.load(open(sketch_path)), skip_front=False, time_frac=0.08)
+            # case 4:
+            #     sketch = drawPNG(json.load(open(sketch_path)), add_stroke=True)
+            case _:
+                sketch = drawPNG(json.load(open(sketch_path)))
 
-        c = self.rng.choice([True, False])
-        if c:
-            sketch = drawPNG(json.load(open(sketch_path)), skip_front=False, time_frac=0.1)
-        else:
-            sketch = drawPNG(json.load(open(sketch_path)))
+        # c = self.rng.choice([True, False])
+        # if c:
+        #     sketch = drawPNG(json.load(open(sketch_path)), skip_front=False, time_frac=0.1)
+        # else:
+        #     sketch = drawPNG(json.load(open(sketch_path)))
 
         # sketch = drawPNG(json.load(open(sketch_path)))
 
