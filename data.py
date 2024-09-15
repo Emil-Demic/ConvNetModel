1fb24bea20a0815c1783f90312ceee84b2ee35be
Emil-Demic <77987406+Emil-Demic@users.noreply.github.com>
2024-09-15 10:48:21 +0200

    Update data.py

--- data.py
+++ data.py
@@ -60,13 +60,13 @@ class DatasetTrain(Dataset):
         #     case _:
         #         sketch = drawPNG(json.load(open(sketch_path)))
 
-        c = self.rng.choice([True, False], p=[0.7, 0.3])
-        if c:
-            sketch = drawPNG(json.load(open(sketch_path)), skip_front=False, time_frac=0.08)
-        else:
-            sketch = drawPNG(json.load(open(sketch_path)))
+        # c = self.rng.choice([True, False], p=[0.7, 0.3])
+        # if c:
+        #     sketch = drawPNG(json.load(open(sketch_path)), skip_front=False, time_frac=0.08)
+        # else:
+        #     sketch = drawPNG(json.load(open(sketch_path)))
 
-        # sketch = drawPNG(json.load(open(sketch_path)))
+        sketch = drawPNG(json.load(open(sketch_path)))
 
         sketch = Image.fromarray(sketch)
         # sketch = ImageOps.pad(sketch, (224, 224), method=Resampling.BILINEAR)
