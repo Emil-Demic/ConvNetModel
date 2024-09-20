dc7dd2920836e31ae73d4f640eae914d8f16a75f
Emil-Demic <77987406+Emil-Demic@users.noreply.github.com>
2024-09-20 10:39:50 +0200

    T1

--- train.py
+++ train.py
@@ -75,7 +75,6 @@ for epoch in range(args.epochs):
 
     print(f"lr: {optimizer.state_dict()['param_groups'][0]['lr']}")
     scheduler.step()
-    # loss_fn.margin += 0.02
 
     with torch.no_grad():
         model.eval()
