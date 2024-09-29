81ae0b7ac6559c60daf531183c989235e82443fa
Emil-Demic <77987406+Emil-Demic@users.noreply.github.com>
2024-09-29 12:25:51 +0200

    vit test

--- model.py
+++ model.py
@@ -16,17 +16,6 @@ def get_network(model: str, pretrained: bool):
                 net = convnext_small().features
             num_features = 768
 
-        case 'swin':
-            from torchvision.models import swin_v2_t
-            if pretrained:
-                from torchvision.models import Swin_V2_T_Weights
-                net = swin_v2_t(weights=Swin_V2_T_Weights.DEFAULT)
-            else:
-                net = swin_v2_t()
-            net.norm = Identity()
-            net.head = Identity()
-            num_features = 768
-
         case 'vit':
             from torchvision.models import vit_b_16
             if pretrained:
@@ -53,17 +42,22 @@ class TripletModel(nn.Module):
     def __init__(self, model, pretrained=True):
         super(TripletModel, self).__init__()
         net_info = get_network(model, pretrained)
+        self.model = model
         self.embedding_net = net_info[0]
         self.num_features = net_info[1]
-        self.pool = AdaptiveAvgPool2d(1)
+        if model == 'vgg16':
+            self.pool = AdaptiveMaxPool2d(1)
+        else:
+            self.pool = AdaptiveAvgPool2d(1)
 
     def forward(self, data):
         res1 = self.embedding_net(data[0])
         res2 = self.embedding_net(data[1])
         res3 = self.embedding_net(data[2])
-        # res1 = self.pool(res1).view(-1, self.num_features)
-        # res2 = self.pool(res2).view(-1, self.num_features)
-        # res3 = self.pool(res3).view(-1, self.num_features)
+        if self.model != "vit":
+            res1 = self.pool(res1).view(-1, self.num_features)
+            res2 = self.pool(res2).view(-1, self.num_features)
+            res3 = self.pool(res3).view(-1, self.num_features)
         res1 = F.normalize(res1)
         res2 = F.normalize(res2)
         res3 = F.normalize(res3)
@@ -71,6 +65,7 @@ class TripletModel(nn.Module):
 
     def get_embedding(self, data):
         res = self.embedding_net(data)
-        # res = self.pool(res).view(-1, self.num_features)
+        if self.model != "vit":
+            res = self.pool(res).view(-1, self.num_features)
         res = F.normalize(res)
         return res
