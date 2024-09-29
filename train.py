56fba8fbc1c16aad1ce08dabf3b56bfe68feca69
Emil-Demic <77987406+Emil-Demic@users.noreply.github.com>
2024-09-29 10:38:41 +0200

    Update train.py

--- train.py
+++ train.py
@@ -54,6 +54,9 @@ loss_fn = InfoNCE(negative_mode="unpaired", temperature=0.05)
 for epoch in range(args.epochs):
     model.train()
     running_loss = 0.0
+    start = torch.cuda.Event(enable_timing=True)
+    end = torch.cuda.Event(enable_timing=True)
+    start.record()
     for i, data in enumerate(dataloader_train):
         optimizer.zero_grad()
         if args.cuda:
@@ -70,6 +73,10 @@ for epoch in range(args.epochs):
         if i % 5 == 4:
             print(f'[{epoch:03d}, {i:03d}] loss: {running_loss / 5:0.5f}')
             running_loss = 0.0
+
+    end.record()
+    torch.cuda.synchronize()
+    print(start.elapsed_time(end) / 1000)
 
     print(f"lr: {optimizer.state_dict()['param_groups'][0]['lr']}")
     scheduler.step()
