e4375aab70387dc65dc5ddea70217c37e45cf0b2
Emil-Demic <77987406+Emil-Demic@users.noreply.github.com>
2024-09-29 12:37:53 +0200

    att fix

--- train.py
+++ train.py
@@ -21,7 +21,7 @@ torch.manual_seed(args.seed)
 if args.cuda:
     torch.cuda.manual_seed(args.seed)
     torch.backends.cudnn.benchmark = False
-    torch.use_deterministic_algorithms(True, warn_only=True)
+    torch.use_deterministic_algorithms(True, warn_only=False)
 
 transforms = Compose([
     RGB(),
