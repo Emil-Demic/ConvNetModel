304b3d14007b8615f4a21e3b7efb46119514f998
Emil-Demic <77987406+Emil-Demic@users.noreply.github.com>
2024-10-07 20:28:07 +0200

    t

--- convnextv2.py
+++ convnextv2.py
@@ -9,7 +9,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from timm.models.layers import trunc_normal_, DropPath
-from .utilsM import LayerNorm, GRN
+from utils import LayerNorm, GRN
 
 class Block(nn.Module):
     """ ConvNeXtV2 Block.
